f4da6caba9f937806b7c2a1f95506c5ecfb1a9f2
ManuIdentiFY <manuel.petit@inserm.fr>
2018-07-05 10:25:28 +0200

    Add launcher

---
+++ launcher.m
@@ -0,0 +1,13 @@
+function launcher()
+
+% Add path
+addpath(genpath('Data Controller'));
+addpath(genpath('Data Storage'));
+addpath(genpath('Data Viewer'));
+addpath(genpath('Toolbox'));
+
+% Create application
+this = FitLike();
+
+end % launcher
+
