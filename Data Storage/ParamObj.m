d0f3aeeb3d74e50071887a140ccb36b5d0d3350c
ManuIdentiFY <manuel.petit@inserm.fr>
2018-07-12 14:25:54 +0200

    Adapt setfield and getfield to array of object (see previous commit)

--- Data Storage/ParamObj.m
+++ Data Storage/ParamObj.m
@@ -1,4 +1,4 @@
-classdef ParamObj
+classdef ParamObj < handle
     
     properties
         paramList@struct;   % list of parameters
@@ -32,14 +32,79 @@ classdef ParamObj
             end
         end
         
+        % Syntax: val = getfield(self, 'field')
+        %         val = getfield(self, 'field', 'ForceCellOutput', 'True')
+        function value = getfield(self, field, varargin)
+            % check if the field exist in all the parameter structures
+            isfld = arrayfun(@(x) isfield(x.paramList,field), self);
+            % check input size
+            if length(self) > 1
+                if all( isfld == 1) 
+                    % get value
+                    value = arrayfun(@(x) x.paramList.(field), self, 'UniformOutput',0);
+                else
+                    % initialise empty cell array
+                    value = cell(1,length(self));
+                    % fill what you can
+                    value(isfld) = arrayfun(@(x) x.paramList.(field), self, 'UniformOutput',0);
+                    % throw a warning
+                    warning('getfield:MissingField',['One or more structure(s)'...
+                                        ' miss the field required'])
+                end
+            else
+                if isfld ~= 1
+                    error('getfield:MissingField',['The required field does'...
+                                         ' not exist'])
+                else
+                    if nargin > 2
+                        if strcmp(varargin{1},'ForceCellOutput') &&...
+                                strcmpi(varargin{2},'true')
+                            value = {self.paramList.(field)};
+                        elseif strcmpi(varargin{2},'false')
+                            value = self.paramList.(field);
+                        else
+                            error('getfield:ForceCellOutput',['Wrong optional argument'...
+                                          ' or value associated'])
+                        end
+                    else                           
+                        value = self.paramList.(field);
+                    end
+                end
+            end
+        end %getfield
         
-        function x = getfield(self,varargin)
-            x = getfield(self.paramList,varargin{:});
-        end
-        
-        function self = setfield(self,varargin)
-            self.paramList = setfield(self.paramList,varargin{:});
-        end
+        function self = setfield(self, field, value)
+            if length(self) > 1 
+                % check if the field exist in all the parameter structures
+                isfld = arrayfun(@(x) isfield(x.paramList,field), self);
+                % check the size of value
+                if ~iscell(value) || length(value) ~= length(self)
+                    error('setfield:WrongSizeInput',['The size of the value input'...
+                        ' does not fit with the size of the array OR value input'...
+                        'is not a cell'])
+                end
+                % because we are working with substructure, a for loop is
+                % required
+                if all(isfld ~= 1)
+                    warning('setfield:MissingField',['One or more structure(s)'...
+                                    ' miss the field required'])
+                    self = self(isfld);
+                    value = value(isfld);
+                end
+                % loop
+                for i = 1:length(self)
+                    self(i).paramList.(field) = value{i};
+                end
+            else
+                % check if the field exist
+                if ~isfield(self.paramList,field)
+                    error('setfield:MissingField',['The field required does'...
+                                          ' not exist'])
+                else
+                    self.paramList.(field) = value;
+                end
+            end
+        end %setfield
         
         function self = changeFieldName(self,old,new)
             self.paramList  = setfield(self.paramList ,new,getfield(self,old));
