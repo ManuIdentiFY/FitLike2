67ab81c3b2cb597323bb645b712c68e0c0ec893a
ManuIdentiFY <manuel.petit@inserm.fr>
2018-07-12 14:26:42 +0200

    ParamV1 constructor
    Same constructor as DataUnit:
    * non cell input --> object
    * cell input --> array of object

--- Data Storage/ParamV1.m
+++ Data Storage/ParamV1.m
@@ -1,14 +1,28 @@
 classdef ParamV1 < ParamObj
     
     properties
-        
-        
+          % See ParamObj properties 
     end
     
     methods
-        function self = ParamV1(varargin)
-            self@ParamObj(varargin);
-        end
+        % parameter can be a structure or a cell array of structure. If a
+        % cell array of structure is detected then PARAMV1 creates an array
+        % of ParamV1 object.
+        function self = ParamV1(parameter)
+            % check input
+            if nargin == 0
+                return
+            end
+            
+            % check if array of struct
+            if ~iscell(parameter)
+                % struct
+                self.paramList = parameter;                           
+            else
+                % array of struct            
+                [self(1:length(parameter)).paramList] = deal(parameter{:});                      
+            end   
+        end %ParamV1
             
         function x = getZoneAxis(self)
         
