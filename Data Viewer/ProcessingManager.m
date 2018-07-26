e7011346248152bafdd29e09a98536bdea6f93ad
ManuIdentiFY <manuel.petit@inserm.fr>
2018-07-26 12:48:30 +0200

    Add ProcessingManager View
    * Just the interface (no callback except show()  and delete() methods)
    * A first attempt to this View
    
    * Need to add custom tab (as DisplayTab) to allow pipeline customisation by user
    * Need to add method to copy the current tree in FileManager inside the File Selection panel

---
+++ Data Viewer/ProcessingManager.m
@@ -0,0 +1,41 @@
+classdef ProcessingManager < handle
+    %
+    % View for ProcessingManager in FitLike
+    %
+    
+    properties
+        gui % GUI (View)
+        FitLike % Presenter
+    end
+    
+    methods
+        % Constructor
+        function this = ProcessingManager(FitLike)
+            %%--------------------------BUILDER--------------------------%%
+            % Store a reference to the presenter
+            this.FitLike = FitLike;
+                      
+            % Make the figure
+            gui = buildProcessingManager();
+            this.gui = guihandles(gui);     
+            
+            %%-------------------------CALLBACK--------------------------%%
+            % Replace the close function by setting the visibility to off
+            set(this.gui.fig,  'closerequestfcn', ...
+                @(src, event) this.FitLike.hideWindowPressed(src));            
+        end %ProcessingManager
+        
+        % Destructor
+        function deleteWindow(this)
+            %remove the closerequestfcn from the figure, this prevents an
+            %infitie loop with the following delete command
+            set(this.gui.fig,  'closerequestfcn', '');
+            %delete the figure
+            delete(this.gui.fig);
+            %clear out the pointer to the figure - prevents memory leaks
+            this.gui = [];
+        end  %deleteWindow 
+    end
+    
+end
+
