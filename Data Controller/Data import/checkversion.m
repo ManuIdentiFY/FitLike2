b13c08859bae87707e638ac9e01b40ca49d10da7
lionelbroche <l.broche@abdn.ac.uk>
2018-10-05 17:34:13 +0100

    sdf version fix
    Adding more conditions to better recognise sdf files version 2

--- Data Controller/Data import/checkversion.m
+++ Data Controller/Data import/checkversion.m
@@ -6,7 +6,9 @@ function ver = checkversion(filename)
 fid = fopen(filename, 'r'); % open the file in read only mode
 line = textscan(fid,'%s',1,'Delimiter','=','HeaderLines',2); %read the first field to check the version
 
-if strcmp(deblank(line{1}),'VERSION') 
+% Version 2 files can start in different ways depending on how the
+% acquisition was performed. Therefore several tests are needed.
+if strcmp(deblank(line{1}),'VERSION') || strcmp(deblank(line{1}),'PARAMETER SUMMARY') || strcmp(deblank(line{1}),'SEQUENCE NAME')
     ver = 2;
 else
     ver = 1;
