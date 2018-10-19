6b9c23e6f2887a11d6f3a500f421846858327f73
lionelbroche <l.broche@abdn.ac.uk>
2018-10-19 09:26:38 +0100

    Fixing problems with units of the X-axis

--- Data Controller/Data processing/Dispersion models/PowerLawTwoSegments.m
+++ Data Controller/Data processing/Dispersion models/PowerLawTwoSegments.m
@@ -7,13 +7,13 @@ classdef PowerLawTwoSegments < DispersionModel
     % adapted for FitLike2, 16/08/2018
 properties
     modelName = '2-segments power law';                             % character string, name of the model, as appearing in the figure legend. You may use spaces here.
-    modelEquation =[ '(dl*f.^v1)*(f<f_trans1) + '...
-                       '((dl*f_trans1^(v1-v2))*f^v2)*(f>f_trans1)'];  % character string, equation that relates the Larmor frequency (MHz) to the parameters to R1 (s^{-1})
+    modelEquation =[ '(dl*f.^vlow)*(f<f_trans1) + '...
+                       '((dl*f_trans1^(vlow-vhigh))*f^vhigh)*(f>f_trans1)'];  % character string, equation that relates the Larmor frequency (MHz) to the parameters to R1 (s^{-1})
     variableName = {'f'};                                           % Characters, name of the variables appearing in the equation (frequency)
-    parameterName = {'dl',  'v1',  'v2', 'f_trans1'};               % List of characters, name of the parameters appearing in the equation
-    minValue =      [0,      -2,    -2,      1e-3];                  % array of values, minimum values reachable for each parameter, respective to the order of parameterName
-    maxValue =      [100,      0,     0,     10];                  % array of values, maximum values reachable for each parameter, respective to the order of parameterName
-    startPoint =    [18,    -0.05, -0.32,    0.5];                % array of values, starting point for each parameter, respective to the order of parameterName 
+    parameterName = {'dl',  'vlow',  'vhigh', 'f_trans1'};               % List of characters, name of the parameters appearing in the equation
+    minValue =      [0,      -2,    -2,      1e3];                  % array of values, minimum values reachable for each parameter, respective to the order of parameterName
+    maxValue =      [100,      0,     0,     10e6];                  % array of values, maximum values reachable for each parameter, respective to the order of parameterName
+    startPoint =    [18,    -0.05, -0.32,    0.5e6];                % array of values, starting point for each parameter, respective to the order of parameterName 
     isFixed =       [0        0       0       0 ];
     
 end
@@ -21,8 +21,8 @@ end
  methods
     function model = PowerLawTwoSegments
         % additional property used to visualise the individual components
-        model.visualisationFunction = {'dl*f.^v1', ...
-                                       '(dl*f_trans1^(v1-v2))*f^v2'};
+        model.visualisationFunction = {'dl*f.^vlow', ...
+                                       '(dl*f_trans1^(vlow-vhigh))*f^vhigh'};
     end 
 
     function self = evaluateStartPoint(self,x,y)
