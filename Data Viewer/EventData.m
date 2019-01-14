c3a71ac49f13dd10431c369f35f8a4af64f8ca1e
ManuIdentiFY <manuel.petit@inserm.fr>
2019-01-14 22:19:13 +0100

    Remove unwanted events

--- Data Viewer/EventData.m
+++ Data Viewer/EventData.m
@@ -1,11 +1,11 @@
-classdef EventData < event.EventData
-   properties
-      idxZone
-   end
-   
-   methods
-      function data = EventData(idxZone)
-         data.idxZone = idxZone;
-      end
-   end
-end
+% classdef EventData < event.EventData
+%    properties
+%       idxZone
+%    end
+%    
+%    methods
+%       function data = EventData(idxZone)
+%          data.idxZone = idxZone;
+%       end
+%    end
+% end
