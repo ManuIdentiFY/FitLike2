27bd730ca6ebdf16dc0c6b40e7d612cf218c17eb
ManuIdentiFY <manuel.petit@inserm.fr>
2019-01-22 17:20:08 +0100

    nothing

--- Data Controller/Data processing/Dispersion fit algorithms/FitAlgorithm.m
+++ Data Controller/Data processing/Dispersion fit algorithms/FitAlgorithm.m
@@ -20,21 +20,5 @@ classdef FitAlgorithm
     methods (Abstract)
         applyFit(this)
     end       
-    
-    methods
-        function this = set.options(this, val)
-            % call check function
-            val = checkOptions(this, val);
-            
-            % set value
-            this.options = val;
-        end
-    end
-    
-    methods (Access = protected)
-        % dummy function that can be redefined in subclasses
-        function val = checkOptions(this,val)            
-        end %checkOptions
-    end
 end
 
