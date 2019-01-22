82fe8731ac89dd857868c6e984e60d8f0509836c
ManuIdentiFY <manuel.petit@inserm.fr>
2019-01-22 17:19:16 +0100

    LsqCurveFit: add get/set options methods()

--- Data Controller/Data processing/Dispersion fit algorithms/FitAlgorithm.m
+++ Data Controller/Data processing/Dispersion fit algorithms/FitAlgorithm.m
@@ -19,6 +19,22 @@ classdef FitAlgorithm
     
     methods (Abstract)
         applyFit(this)
-    end         
+    end       
+    
+    methods
+        function this = set.options(this, val)
+            % call check function
+            val = checkOptions(this, val);
+            
+            % set value
+            this.options = val;
+        end
+    end
+    
+    methods (Access = protected)
+        % dummy function that can be redefined in subclasses
+        function val = checkOptions(this,val)            
+        end %checkOptions
+    end
 end
 
