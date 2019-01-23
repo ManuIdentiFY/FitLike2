471ed14fca576cf30d7763b83816f198e40e2d3e
lionelbroche <l.broche@abdn.ac.uk>
2019-01-23 17:08:49 +0000

    ProcessDataUnit: passing the processDataFunction from DataUnit2DataUnit

--- Data Controller/Data processing/ProcessDataUnit.m
+++ Data Controller/Data processing/ProcessDataUnit.m
@@ -1,4 +1,4 @@
-classdef ProcessDataUnit < matlab.mixin.Heterogeneous
+classdef ProcessDataUnit < handle
 % this class defines the structure and general attibute of all the objects
 % that are used to process the data units. It aims at facilitating
 % operations on the processing functions, to streamline the creation of
@@ -36,25 +36,115 @@ classdef ProcessDataUnit < matlab.mixin.Heterogeneous
         function out = selfCheck(self)
             out = 1;
         end
+%         
+%         % main process function
+%         function [childObj, parentObj] = processData(this, parentObj)
+%             % get data
+%             data = getProcessData(this, parentObj);
+%             
+%             % apply process
+%             [this, new_data] = arrayfun(@(x) applyProcess(this, x, parentObj), data, 'Uniform', 0);
+%             
+%             % gather data and create childObj
+%             childObj = makeProcessData(this, new_data, parentObj);           
+%         end %processData
+%         
+%         
         
-        % main process function
-        function [childObj, parentObj] = processData(this, parentObj)
-            % get data
-            data = getProcessData(this, parentObj);
+                
+        % function that applies one processing function to one bloc only.
+        % This is where the custom processing function is being called.
+        function [outputdata,inputdata] = applyProcessFunction(this)  
             
-            % apply process
-            [this, new_data] = arrayfun(@(x) applyProcess(this, x, parentObj), data, 'Uniform', 0);
+            sze = size(this.InputData.y);
+            if length(sze)<3
+                sze(3) = 1;   % make sure the data is interpreted as a 3D matrix
+            end
+            % prepare the cell arrays, making sure the dimensions are
+            % consistent
+            cellx = squeeze(num2cell(this.InputData.x,1));
+            celly = squeeze(num2cell(this.InputData.y,1));
+            % make sure the data is sorted
+            [cellx,ord] = cellfun(@(c)sort(c),cellx,'UniformOutput',0);
+            celly = cellfun(@(c,o)c(o),celly,ord,'UniformOutput',0);
+            % cast to cell array for cellfun
+%             cellindex = repmat(num2cell(1:this.InputData.parameter.paramList.NBLK)',1,size(this.InputData.y,3));
+            if isempty(this.InputData.y)
+                z = [];
+                dz = [];
+                this.ProcessData = {};
+            else
+                for i = 1:getRelaxProp(this.InputData, 'NBLK')                
+                    for j = 1:size(this.InputData.y,3)
+                        cellindex{i,j} = [i,j]; %#ok<AGROW>
+                    end
+                end
+                if ~isequal(size(cellindex),size(cellx))
+                    cellx = cellx';
+                    celly = celly';
+                end
+                % make sure that each acquisition is referenced from the time
+                % of acquisition within the data this.InputData
+                cellx = cellfun(@(x)x-x(1),cellx,'UniformOutput',0);
+                % process the cell array to get the this.OutputData data
+                [z, dz, this.ProcessData] = cellfun(@(x,y,i) process(this,x,y,this.InputData,i),cellx,celly,cellindex,'Uniform',0);
+                szeout = size(z{1,1});
+                [szeout,ind] = max(szeout); 
+                if ind == 2 % check that the result of the process is a column array
+                    z = reshape(cell2mat(z),sze(2),szeout,sze(3));
+                    dz = reshape(cell2mat(dz),sze(2),szeout,sze(3));
+                else
+                    z = reshape(cell2mat(z),szeout,sze(2),sze(3));
+                    z = permute(z,[2 1 3]);
+                    dz = reshape(cell2mat(dz),szeout,sze(2),sze(3));
+                    dz = permute(dz,[2 1 3]);
+                end
+            end
             
-            % gather data and create childObj
-            childObj = makeProcessData(this, new_data, parentObj);           
-        end %processData
+            % generate one this.OutputData object for each component provided by the
+            % processing algorithm
+            warning('off','MATLAB:mat2cell:TrailingUnityVectorArgRemoved') % avoid spamming the terminal when the data is not multiexponential
+            cellz = mat2cell(z,size(z,1),ones(1,size(z,2)),size(z,3));
+            celldz = mat2cell(dz,size(dz,1),ones(1,size(dz,2)),size(dz,3));
+            cellz = cellfun(@(x) squeeze(x),cellz,'UniformOutput',0);
+            celldz = cellfun(@(x) squeeze(x),celldz,'UniformOutput',0);
+            x = getZoneAxis(this.InputData); % raw x-axis (needs to be repmat to fit the dimension of y)
+            x = repmat(x,size(cellz)); % make sure that all cell arrays are consistent
+%             params = repmat({params},size(cellz));
+            labelX = repmat({this.labelX},size(cellz));
+            labelY = repmat({this.labelY},size(cellz));
+            if numel(this.legendTag) ~= numel(labelX)
+                legendTag = repmat(this.legendTag,size(cellz));
+            else
+                legendTag = this.legendTag;
+            end
+            
+            % generate the children objects if they are not yet created
+            if isempty(this.OutputData)
+                this.OutputData = Zone('parent',repmat({this.InputData},size(celldz)),...
+                                       'x',x,'xLabel',labelX,...
+                                       'y',cellz,'dy',celldz,'yLabel',labelY,...
+                                       'legendTag',legendTag,...
+                                       'relaxObj',this.InputData.relaxObj);
+            else % if a child object is there, just update it
+                this.OutputData = arrayfun(@(z,lx,cz,cdz,ly,l) updateProperties(z,...
+                                            'xLabel',lx,...
+                                            'y',cz,'dy',cdz,'yLabel',ly,...
+                                            'legendTag',l),...
+                                            this.OutputData,labelX,cellz,celldz,labelY,legendTag);
+            end
+            outputdata = this.OutputData;
+            inputdata = this.InputData;
+        end
+
+        
     end
     
-    methods (Abstract)
-        function [this, new_data] = applyProcess(this, data, parentObj)
-            
-        end %applyProcess
-    end
+%     methods (Abstract)
+%         function [this, new_data] = applyProcess(this, data, parentObj)
+%             
+%         end %applyProcess
+%     end
 %       
 %         % function that allows estimating the start point. It should be 
 %         % over-riden by the derived classes
