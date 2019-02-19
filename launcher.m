bbf7bf7dd9b2e830a2c5714c2c42814069ff738f
ManuIdentiFY <manuel.petit@inserm.fr>
2019-02-19 16:46:50 +0100

    set the path for dataset test

--- launcher.m
+++ launcher.m
@@ -5,6 +5,7 @@ addpath(genpath('Data Controller'));
 addpath(genpath('Data Storage'));
 addpath(genpath('Data Viewer'));
 addpath(genpath('Toolbox'));
+addpath(genpath('dataset test'));
 
 
 % Create application
