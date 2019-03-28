e516f21c0ff9f27a3244025ff93565880d890908
ManuIdentiFY <manuel.petit@inserm.fr>
2019-03-28 22:37:43 +0100

    remove dataset test path

--- launcher.m
+++ launcher.m
@@ -5,7 +5,6 @@ addpath(genpath('Data Controller'));
 addpath(genpath('Data Storage'));
 addpath(genpath('Data Viewer'));
 addpath(genpath('Toolbox'));
-addpath(genpath('dataset test'));
 
 % Create application
 this = FitLike();
