a46feb94947232da70d8f5a812af4426eb3c7ea4
ManuIdentiFY <manuel.petit@inserm.fr>
2019-04-01 10:26:35 +0200

    Add setup function to add path separately

--- launcher.m
+++ launcher.m
@@ -1,10 +1,7 @@
 function launcher()
 
 % Add path
-addpath(genpath('Data Controller'));
-addpath(genpath('Data Storage'));
-addpath(genpath('Data Viewer'));
-addpath(genpath('Toolbox'));
+setup();
 
 % Create application
 this = FitLike();
---
+++ setup.m
@@ -0,0 +1,10 @@
+function setup()
+%
+% This function adds the path required for FitLike.
+%
+addpath(genpath('Data Controller'));
+addpath(genpath('Data Storage'));
+addpath(genpath('Data Viewer'));
+addpath(genpath('Toolbox'));
+end
+
