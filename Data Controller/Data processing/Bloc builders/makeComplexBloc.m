fef60d59f0ca418d70208e21bcca79b2a040ec11
lionelbroche <l.broche@abdn.ac.uk>
2019-04-22 13:51:35 +0100

    fixing old code for compatiblity
    removing the parameter handling from the bloc processing function

--- Data Controller/Data processing/Bloc builders/makeComplexBloc.m
+++ Data Controller/Data processing/Bloc builders/makeComplexBloc.m
@@ -1,4 +1,4 @@
-function blocList = makeComplexBloc(data,param) 
+function blocList = makeComplexBloc(data) 
 % This class accepts the output from file readers and turn them into
 % Bloc objects.
 % make a list of blocs from data arrays obtained from the file
@@ -7,7 +7,7 @@ function blocList = makeComplexBloc(data,param)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 % Remove parameter or change data access [Manu]
 blocList = Bloc('y',cellfun(@(x,y) x+1i*y,data.real,data.imag,'UniformOutput',0),'x',data.time);
-for i = 1:length(blocList)
-    blocList(i).parameter = param(i);
-end
+% for i = 1:length(blocList)
+%     blocList(i).parameter = param(i);
+% end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
